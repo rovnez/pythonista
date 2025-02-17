e6b673d07f5e5d6811dd2a3baa40f7ad0aed17e5
rovnez <rovnez42@gmail.com>
2025-02-17 21:30:49 +0100

    Initial commit

---
+++ src/pythonista/dummy.py
@@ -0,0 +1,13 @@
+
+
+class Dummy:
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
